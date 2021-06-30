e3f53702f8f960511927d0d3de395588cedf68bf
Ezequiel Guitler <eguitler@gmail.com>
2021-06-30 20:25:43 -0300

    Movie's border styles fixed

--- src/components/Movie/MovieStyles.jsx
+++ src/components/Movie/MovieStyles.jsx
@@ -2,20 +2,14 @@ import styled from "styled-components";
 
 export const StyledMovie = styled.div`
     position: relative;
-    /* height: 100%; */
-    /* width: 100%; */
-    /* max-width: 420px; */
-    border: 2px solid;
-        cursor: pointer;
-
+    cursor: pointer;
 
     & .movie-cover {
         width: 100%;
         object-fit: cover;
         user-select: none;
         border-radius: 5px;
-
-        /* transition: transform 500ms; */
+        border: 2px solid rgba(255, 255, 255, .4);
     }
 
     & > p {
