2892ab613d557a6208793faefbef89f967eb60a3
Ezequiel Guitler <eguitler@gmail.com>
2021-07-05 16:32:35 -0300

    srcSet type fixed and reformat

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,7 +1,7 @@
-import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { StyledMovie } from "./MovieStyles";
 import MoviePreviewMiniModal from "components/MoviePreviewMiniModal/MoviePreviewMiniModal";
+import { getMovieDetails } from "services/movies";
 
 const Movie = ({ id, src, srcset }) => {
     const [movieData, setMovieData] = useState(null);
@@ -18,19 +18,8 @@ const Movie = ({ id, src, srcset }) => {
     };
 
     useEffect(() => {
-        const fetchingData = async (with_images = false, with_cast = false) => {
-            const baseURL = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}&with_images=${with_images}&with_cast=${with_cast}`;
-            const response = await axios.get(baseURL);
-            const data = await response.data;
-            return data.data.movie;
-        };
-
         if (hover && !movieData) {
-            try {
-                fetchingData(true, true).then((data) => setMovieData(data));
-            } catch (err) {
-                console.log("err: ", err);
-            }
+            getMovieDetails(id).then((data) => setMovieData(data));
         }
     }, [hover, id, movieData]);
     return (
@@ -39,15 +28,21 @@ const Movie = ({ id, src, srcset }) => {
             onMouseLeave={() => setHover(false)}
             ref={movieRef}
         >
-            <img className="movie-cover" src={src} srcset={srcset} alt="" loading="lazy" />
-            {hover &&
+            <img
+                className="movie-cover"
+                src={src}
+                srcSet={srcset}
+                alt=""
+                loading="lazy"
+            />
+            {hover && (
                 <MoviePreviewMiniModal
                     active={hover}
                     itemH={getHeight()}
                     itemW={getWidth()}
                     movieData={movieData}
                 />
-            }
+            )}
         </StyledMovie>
     );
 };
