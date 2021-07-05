6511a5148e97f96206cf371991b5a8e20549f107
Ezequiel Guitler <eguitler@gmail.com>
2021-07-05 17:43:06 -0300

    MoviePreviewMiniModal is responsive now!

--- src/components/MoviePreviewMiniModal/MoviePreviewMiniModalStyles.jsx
+++ src/components/MoviePreviewMiniModal/MoviePreviewMiniModalStyles.jsx
@@ -103,7 +103,8 @@ export const StyledMiniModal = styled.div`
             justify-content: space-between;
             --invertColor: invert(37%) sepia(99%) saturate(548%)
                 hue-rotate(210deg) brightness(79%) contrast(96%);
-            --inverGreenColor:  invert(44%) sepia(63%) saturate(740%) hue-rotate(71deg) brightness(111%) contrast(87%);
+            --inverGreenColor: invert(44%) sepia(63%) saturate(740%)
+                hue-rotate(71deg) brightness(111%) contrast(87%);
 
             & .icon {
                 height: 50px;
@@ -116,14 +117,15 @@ export const StyledMiniModal = styled.div`
                     &:hover {
                         filter: invert(62%) sepia(48%) saturate(586%)
                             hue-rotate(204deg) brightness(103%) contrast(109%);
-                        opacity: 0.9;   
+                        opacity: 0.9;
                     }
                 }
 
                 &.tick {
                     filter: var(--inverGreenColor);
-                    &:hover{
-                        filter: invert(79%) sepia(36%) saturate(3915%) hue-rotate(64deg) brightness(97%) contrast(73%);
+                    &:hover {
+                        filter: invert(79%) sepia(36%) saturate(3915%)
+                            hue-rotate(64deg) brightness(97%) contrast(73%);
                     }
                 }
             }
@@ -184,6 +186,109 @@ export const StyledMiniModal = styled.div`
             font-size: 16px;
             transform: scale(1);
             opacity: 1;
+        }
+    }
+
+    @media screen and (max-width: 1024px) {
+        border-top: 2px solid #666;
+        border-radius: 0;
+        box-shadow: 0 -5px 15px rgba(0, 0, 0, 0.6);
+
+        &.active {
+            position: fixed;
+            flex-direction: row;
+            top: auto;
+            right: 0;
+            left: 0;
+            bottom: 0;
+            height: 300px;
+            width: 100%;
+            background-color: #2a2a2a;
+
+            animation: appearFromBottom 0.2s 0.5s forwards;
+
+            @keyframes appearFromBottom {
+                0% {
+                    transform: translateY(100%);
+                }
+                10% {
+                    opacity: 0.5;
+                }
+                100% {
+                    transform: translateY(0);
+                    opacity: 1;
+                }
+            }
+        }
+
+        & .trailer-wrapper {
+            height: auto;
+            background-color: #2a2a2a;
+            margin: 20px;
+            border-radius: 3px;
+        }
+
+        & .info-description {
+            height: 100%;
+            padding: 20px 0;
+
+            & .title-genres-wrapper {
+                & .title {
+                    font-size: 2em;
+                }
+                & .year-genres {
+                    font-size: 1.2em;
+                }
+            }
+
+            & .description {
+                height: 100%;
+
+                & p {
+                    -webkit-line-clamp: 5;
+                    font-size: 1.1rem;
+                }
+            }
+            & .buttons-wrapper {
+                display: flex;
+                justify-content: start;
+                gap: 15px;
+            }
+        }
+    }
+
+    @media screen and (max-width: 768px) {
+        &.active {
+            height: 250px;
+        }
+
+        & .trailer-wrapper {
+            position: absolute;
+            bottom: 100%;
+            left: 0;
+            height: auto;
+            background-color: #2a2a2a;
+            margin: auto;
+            border-radius: 3px;
+        }
+
+        & .info-description {
+            padding: 20px;
+
+            & .description {
+                height: 100%;
+
+                & p {
+                    -webkit-line-clamp: 3;
+                    font-size: 1.1rem;
+                }
+            }
+        }
+    }
+
+    @media screen and (max-width: 480px) {
+        & .trailer-wrapper {
+            display: none;
         }
     }
 `;
